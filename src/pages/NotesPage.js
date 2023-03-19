92a9c4ab36d956edad4ecf2060307cb0ee8148e2
AYAZ <ayazmohdkhan21@gmail.com>
2023-03-20 01:41:24 +0530

    notes page mobile view

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -84,7 +84,7 @@ const NotesPage = (props) => {
 					<img alt="" src={imageUrl}></img>
 				</div>
 			</main>
-			<aside className={aside_class}>
+			<aside className={aside_class + " aside_mobile"}>
 				<img
 					alt=""
 					onClick={handler}
